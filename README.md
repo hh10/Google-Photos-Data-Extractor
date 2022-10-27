3431c5b52e3e024e274e4fe4186375a71c2ad46e
hh <hh@daedalean.ai>
2022-10-27 18:45:32 +0100

    adding comments

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Google Photos Data Extractor
-A very primitive go module to extract metadata of media in Google Photo albums. The module uses [Google Photos Library API](https://developers.google.com/photos/library/reference/rest) and needs one of [these Authorization Scopes](https://developers.google.com/photos/library/reference/rest/v1/albums/list#authorization-scopes).
+A basic go module to extract metadata of media in Google Photo albums. The module uses [Google Photos Library API](https://developers.google.com/photos/library/reference/rest) and needs one of [these Authorization Scopes](https://developers.google.com/photos/library/reference/rest/v1/albums/list#authorization-scopes).
 
 ## Usage
-- Setting up OAuth 2.0 to access Google Photos:
-    Go to [API & services menu in Google developers console](https://console.cloud.google.com/apis), create a project, enable Photos Library API for the project, create OAuth credentials for the project (set project type to Desktop), download the client_secret JSON file for the credentials.
+- Setting up OAuth2.0 to access Google Photos:
+    - Go to [API & services menu in Google developers console](https://console.cloud.google.com/apis)
+    - Create a project (or use an existing project), enable Photos Library API for the project.
+    - Create OAuth credentials for the project (set project type to Desktop, *note that currently [main.go](main.go) assumes that the credentials are for a Desktop app client. For another client type, change the json tag accordingly in the Credential struct*), and download the client_secret JSON file for the credentials.
+    These credentials include the client id and secret from the AuthServer, which this application uses to identify itself to it and get authenticated.
 - Install Golang as per [installation instructions](https://go.dev/doc/install).
 - Clone the repo and run
 ```
@@ -11,5 +14,8 @@ cd google_photos_data_extractor
 go get
 go run main.go --client_secret_file=<path to the client_secret.json file you downloaded above>
 ```
-- Follow the instructions at the prompt, i.e., open the suggested link, login with your gmail.
+- Open the link printed at the prompt, login with your gmail and wait to be redirected to a webpage showing a list of photo albums owned by your gmail account. Basically, the opening of the suggested link makes the AuthServer:
+    - authenticate this application and verify that its requested scopes (readonly here) are permitted by gphotoslibrary, 
+    - communicate with gphotoslibrary to get an authorization code,
+    - send this code to a redirect URL that exchanges it for Access and Refresh Tokens (latter not used here) to be used by the application for making further requests to the gphotoslibrary.
 - Select desired album from the available albums in your Google Photos and click on `Fetch Media Metadata` button to download a CSV file with filename, creation times and URL of the media items in the album. Open any URL in browser to view the media file.
--- main.go
+++ main.go
@@ -32,7 +32,6 @@ type Credentials struct {
 }
 
 var conf *oauth2.Config
-var state string
 var token *oauth2.Token
 
 func randToken() string {
@@ -41,6 +40,7 @@ func randToken() string {
 	return base64.StdEncoding.EncodeToString(b)
 }
 
+// handler that exchanges the authorization code provided by AuthServer for Access Token
 func authHandler(w http.ResponseWriter, r *http.Request) {
 	var err error
 	token, err = conf.Exchange(oauth2.NoContext, r.URL.Query().Get("code"))
@@ -51,6 +51,7 @@ func authHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, fmt.Sprintf("http://127.0.0.1%s/", *web), http.StatusSeeOther)
 }
 
+// handler that uses a OAuth2 client with Access Token to return list of all albums owned by a gphotos account
 func listHandler(w http.ResponseWriter, r *http.Request) {
 	client := conf.Client(oauth2.NoContext, token)
 	resp, err := client.Get("https://photoslibrary.googleapis.com/v1/albums")
@@ -81,9 +82,9 @@ func listHandler(w http.ResponseWriter, r *http.Request) {
 	template.Must(template.ParseFiles("data.html")).Execute(w, templateVars)
 }
 
+// handler that uses a OAuth2 client with Access Token to return list of all mediaItems in a requested gphotos album
 func albumHandler(w http.ResponseWriter, r *http.Request) {
-	pathsubs := strings.Split(r.URL.Path, "/")
-	albumId := pathsubs[2]
+	albumId := strings.Split(r.URL.Path, "/")[2]
 	client := conf.Client(oauth2.NoContext, token)
 	values := map[string]string{"albumId": albumId}
 	json_data, err := json.Marshal(values)
@@ -103,6 +104,7 @@ func albumHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	flag.Parse()
 
+	// get credentials from the secrets file
 	secrets_file, err := ioutil.ReadFile(*clientSecretFile)
 	if err != nil {
 		log.Printf("Error in reading client secret file: %v\n", err)
@@ -111,16 +113,24 @@ func main() {
 	cred := Credentials{}
 	json.Unmarshal(secrets_file, &cred)
 
+	// prepare the oauth2 config with credentials from authorization server
 	conf = &oauth2.Config{
 		ClientID:     cred.Installed.ClientID,
 		ClientSecret: cred.Installed.ClientSecret,
-		RedirectURL:  fmt.Sprintf("http://127.0.0.1%s/auth", *web),
+		RedirectURL:  fmt.Sprintf("http://127.0.0.1%s/auth", *web), // url to send the authorization code to
 		Scopes: []string{
 			"https://www.googleapis.com/auth/photoslibrary.readonly", // list of all scopes here: https://developers.google.com/identity/protocols/oauth2/scopes
 		},
 		Endpoint: google.Endpoint,
 	}
-	state = randToken()
+	// NOTE: the implemention here is not secure in that the random state sent to the authorization server when requesting for interactive authentication, is not stored
+	// in local or session storage for validating browser requests. It is easy enough to maintain state in storage and compare the set state here and the states in received
+	// requests in the handler functions, but this example is deliberately kept minimal.
+	state := randToken()
+	// opening of the prompted link in the browser makes the AuthServer:
+	// - authenticate this application and verify that the scopes requested in OAuth2 config are permitted,
+	// - communicate with the resource owner (photoslibrary) to get an authorization code,
+	// - send the authorization code to the redirect URL specified in the OAuth2 config.
 	fmt.Printf("Open the following link in your browser, login and grant permission to the app to access your Google Photos to proceed.\n\n%s", conf.AuthCodeURL(state))
 
 	http.HandleFunc("/", listHandler)
